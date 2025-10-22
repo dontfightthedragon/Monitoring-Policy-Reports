df2917045bdeb309e1eb3f9e8a602c0630977a62
Johanna Theresa Hofmann <115934315+dontfightthedragon@users.noreply.github.com>
2025-10-22 10:59:08 +0200

    Delete scripts directory

--- scripts/evaluate_alignment.py
+++
@@ -1,9 +0,0 @@
-
-import argparse, pandas as pd
-from bio_thesis.eval.metrics import classification_report
-ap = argparse.ArgumentParser()
-ap.add_argument('--pred'); ap.add_argument('--gold')
-args = ap.parse_args()
-pred = pd.read_csv(args.pred); gold = pd.read_csv(args.gold)
-df = gold.merge(pred, on='id')
-print(classification_report(df['y_true'], df['y_pred'], digits=3))
--- scripts/run_topic_model.py
+++
@@ -1,9 +0,0 @@
-
-import argparse, pandas as pd, joblib
-from bio_thesis.models.topic_model import run_bertopic
-ap = argparse.ArgumentParser(); ap.add_argument('--in', dest='inp', required=True)
-ap.add_argument('--text-col', default='DESCRIPTION'); ap.add_argument('--out', dest='out', required=True)
-args = ap.parse_args()
-df = pd.read_parquet(args.inp) if args.inp.endswith('.parquet') else pd.read_csv(args.inp)
-model = run_bertopic(df[args.text_col].astype(str).tolist())
-joblib.dump(model, args.out + '/bertopic.joblib'); print('Model saved')
